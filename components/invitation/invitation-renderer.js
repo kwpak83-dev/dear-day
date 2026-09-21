675bd310d1a451a2e3830a1c3ed029a34cf324a7
kwpak83-dev <kwpak83@gmail.com>
2026-09-21 13:37:11 +0900

    Add template config normalization foundation

--- components/invitation/invitation-renderer.js
+++ components/invitation/invitation-renderer.js
@@ -2,6 +2,7 @@ import ClassicTemplate from "./templates/classic-template";
 import ModernTemplate from "./templates/modern-template";
 import RomanticTemplate from "./templates/romantic-template";
 import { getInvitationPresentation } from "./presentation";
+import { normalizeTemplateConfig } from "../../lib/template-config";
 
 export const TEMPLATE_IDS = {
   classic: "10000000-0000-4000-8000-000000000001",
@@ -15,8 +16,9 @@ export const TEMPLATE_REGISTRY = {
   [TEMPLATE_IDS.modern]: ModernTemplate,
 };
 
-export default function InvitationRenderer({ invitation, eventKind, templateId, placeActions, children }) {
+export default function InvitationRenderer({ invitation, eventKind, templateId, templateConfig, placeActions, children }) {
   const Template = TEMPLATE_REGISTRY[templateId] || ClassicTemplate;
   const presentation = getInvitationPresentation(invitation, eventKind);
-  return <Template presentation={presentation} eventKind={eventKind || invitation.eventKind || "wedding"} placeActions={placeActions}>{children}</Template>;
+  const normalizedConfig = normalizeTemplateConfig(templateConfig);
+  return <Template presentation={presentation} templateConfig={normalizedConfig} eventKind={eventKind || invitation.eventKind || "wedding"} placeActions={placeActions}>{children}</Template>;
 }
