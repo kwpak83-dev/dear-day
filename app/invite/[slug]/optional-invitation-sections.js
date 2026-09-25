b8516220b950165364d8f6b7ef6b6be2f5410ad8
kwpak83-dev <kwpak83@gmail.com>
2026-09-25 22:26:32 +0900

    Restore app/invite/[slug]/optional-invitation-sections.js before preview quick-menu experiments

--- app/invite/[slug]/optional-invitation-sections.js
+++ app/invite/[slug]/optional-invitation-sections.js
@@ -4,8 +4,8 @@ import RsvpForm from "./rsvp-form";
 import Guestbook from "./guestbook";
 import InvitationQuickMenu from "./invitation-quick-menu";
 
-export default function OptionalInvitationSections({ invitation, slug = "", startsAt, preview = false, previewRoot = null, alwaysVisible = false, embedded = false }) {
-  if (!preview) return <InvitationQuickMenu invitation={invitation} slug={slug} startsAt={startsAt} previewRoot={previewRoot} alwaysVisible={alwaysVisible} embedded={embedded} />;
+export default function OptionalInvitationSections({ invitation, slug = "", startsAt, preview = false }) {
+  if (!preview) return <InvitationQuickMenu invitation={invitation} slug={slug} startsAt={startsAt} />;
   return <>
     {invitation.rsvpEnabled === true && <RsvpForm slug={slug} startsAt={startsAt} preview />}
     {invitation.guestbookEnabled !== false && <Guestbook slug={slug} preview />}
