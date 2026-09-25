6b45cf4ce02f811ae36a66519a9512526fda4faf
kwpak83-dev <kwpak83@gmail.com>
2026-09-25 22:00:11 +0900

    Pass embedded preview context to quick menu

--- app/invite/[slug]/optional-invitation-sections.js
+++ app/invite/[slug]/optional-invitation-sections.js
@@ -4,8 +4,8 @@ import RsvpForm from "./rsvp-form";
 import Guestbook from "./guestbook";
 import InvitationQuickMenu from "./invitation-quick-menu";
 
-export default function OptionalInvitationSections({ invitation, slug = "", startsAt, preview = false }) {
-  if (!preview) return <InvitationQuickMenu invitation={invitation} slug={slug} startsAt={startsAt} />;
+export default function OptionalInvitationSections({ invitation, slug = "", startsAt, preview = false, previewRoot = null, alwaysVisible = false }) {
+  if (!preview) return <InvitationQuickMenu invitation={invitation} slug={slug} startsAt={startsAt} previewRoot={previewRoot} alwaysVisible={alwaysVisible} />;
   return <>
     {invitation.rsvpEnabled === true && <RsvpForm slug={slug} startsAt={startsAt} preview />}
     {invitation.guestbookEnabled !== false && <Guestbook slug={slug} preview />}
