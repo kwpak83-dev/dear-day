d5db6fb5a8a14ac92f9b7efbb8d4a7eb2d1d8689
kwpak83-dev <kwpak83@gmail.com>
2026-09-25 23:04:54 +0900

    Pass quick menu preview mode

--- app/invite/[slug]/optional-invitation-sections.js
+++ app/invite/[slug]/optional-invitation-sections.js
@@ -4,8 +4,8 @@ import RsvpForm from "./rsvp-form";
 import Guestbook from "./guestbook";
 import InvitationQuickMenu from "./invitation-quick-menu";
 
-export default function OptionalInvitationSections({ invitation, slug = "", startsAt, preview = false }) {
-  if (!preview) return <InvitationQuickMenu invitation={invitation} slug={slug} startsAt={startsAt} />;
+export default function OptionalInvitationSections({ invitation, slug = "", startsAt, preview = false, previewMode = "" }) {
+  if (!preview) return <InvitationQuickMenu invitation={invitation} slug={slug} startsAt={startsAt} previewMode={previewMode} />;
   return <>
     {invitation.rsvpEnabled === true && <RsvpForm slug={slug} startsAt={startsAt} preview />}
     {invitation.guestbookEnabled !== false && <Guestbook slug={slug} preview />}
